796e8208b69c09a72aec25d043d7ac7de3a70896
Dimitris Exarchou <exarchou.dimitris@gmail.com>
2021-10-14 18:02:43 +0200

    script update

--- Arduino-Distance/Arduino-Distance.ino
+++ Arduino-Distance/Arduino-Distance.ino
@@ -1,32 +1,101 @@
-#define trigPin 13
-#define echoPin 12
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-void setup() 
+#define echoPin 8 //Echo pin
+#define trigPin 9 //Trigger pin
+
+#define led1 A0 //red led 1
+#define led2 A1 //red led 2
+#define led3 A2 //yellow led 3
+#define led4 A3 //yellow led 4
+#define led5 A4 //green led 5
+#define led6 A5 //green led 6
+
+float time;
+float distance_cm;
+float distance_in;
+ 
+void setup()
 {
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+
+  lcd.begin(16, 2);
+  delay(1000);
+  lcd.clear();
 }
-
-
-void loop() 
+ 
+void loop()
 {
-  long duration, distance;
-  digitalWrite(trigPin, LOW);        
-  delayMicroseconds(2);              
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);           
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
-  if (distance >= 200 || distance <= 0)
-  {
-    Serial.println("Out of range");
+  delayMicroseconds(10);
+
+  time = pulseIn(echoPin, HIGH);
+
+  distance_cm = (time/2) / 29.1; 
+  distance_in = (time/2) / 73.914; 
+
+  lcd.setCursor(0, 0);
+  lcd.print("Distance   ");
+
+  lcd.setCursor(0, 1);
+  lcd.print(distance_cm,1);
+  lcd.print("cm  ");
+
+  lcd.setCursor(9, 1);
+  lcd.print(distance_in);
+  lcd.print("in  ");
+
+  Serial.print("cm= ");
+  Serial.print(distance_cm);
+  Serial.print("inch= ");
+  Serial.println(distance_in);
+
+
+  if(distance_cm<60){
+    digitalWrite(led6, HIGH);
+  }else{
+    digitalWrite(led6, LOW);
   }
-  else 
-  {
-    Serial.print(distance);
-    Serial.println(" cm");
+
+  if(distance_cm<50){
+    digitalWrite(led5, HIGH);
+  }else{
+    digitalWrite(led5, LOW);
   }
-  delay(500);
+
+  if(distance_cm<40){
+    digitalWrite(led4, HIGH);
+  }else{
+    digitalWrite(led4, LOW);
+  }
+
+  if(distance_cm<30){
+    digitalWrite(led3, HIGH);
+  }else{
+    digitalWrite(led3, LOW);
+  }
+
+  if(distance_cm<20){
+    digitalWrite(led2, HIGH);
+  }else{
+    digitalWrite(led2, LOW);
+  }
+
+  if(distance_cm<10){
+    digitalWrite(led1, HIGH);
+  }else{
+    digitalWrite(led1, LOW);
+  }
+
+  delay(200);
 }
